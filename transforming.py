ab18655df243c5db20704119a2b9e3972fa733d0
avagadro <p-avagadro@yandex.com>
2025-03-23 08:21:50 +0300

    LogReg rev 6.7 [0.81630]

--- transforming.py
+++ transforming.py
@@ -20,6 +20,7 @@ from typing import (
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 
 import scipy as sp
 from tqdm import tqdm
@@ -373,7 +374,7 @@ class Bins(TransformerMixin):
 
 class KeepDataframe(TransformerMixin):
     """Keep result as DataFrame"""
-    def __init__(self, transformer: TransformerMixin):
+    def __init__(self, transformer: TransformerMixin | Pipeline):
         self._transformer = transformer
 
     def fit(self, X: pd.DataFrame, y: pd.Series | None = None) -> Self:
